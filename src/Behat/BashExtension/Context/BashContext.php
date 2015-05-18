6fb5a70fbd96ede68526b3436c4a11362b9bf8a4
lauft <thomas.lauf@tngtech.com>
2015-05-18 22:20:16 +0200

    Skeleton files

---
+++ src/Behat/BashExtension/Context/BashContext.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Lauft\Behat\BashExtension\Context;
+
+/**
+ * BashContext context for Behat BDD tool.
+ * Provides bash base step definitions.
+ */
+class BashContext extends RawBashContext
+{
+
+}
---
+++ src/Behat/BashExtension/Context/RawBashContext.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Lauft\Behat\BashExtension\Context;
+
+use Behat\Behat\Context\Context
+
+/**
+ * Raw bash context for Behat BDD tool.
+ */
+class RawBashContext implements Context
+{
+
+}
