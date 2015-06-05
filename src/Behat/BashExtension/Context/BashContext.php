4f04084f1438418c720287e9a6707b54c26b8c7b
Thomas Lauf <thomas.lauf@tngtech.com>
2015-06-05 07:50:22 +0200

    Add function to execute arbitrary shell command

--- src/Behat/BashExtension/Context/BashContext.php
+++ src/Behat/BashExtension/Context/BashContext.php
@@ -8,5 +8,19 @@ namespace Lauft\Behat\BashExtension\Context;
  */
 class BashContext extends RawBashContext
 {
+    /**
+     * @When /^I run "([^"]*)"(?: with "([^"]*)")?$/
+     *
+     * @param string $command
+     * @param string $arguments
+     */
+    public function iRunCommand($command, $arguments = '')
+    {
+        $arguments = strtr($arguments, array('\'' => '"'));
 
+        $this->process->setWorkingDirectory($this->workingDir);
+        $this->process->setCommandLine($command.' '.$arguments);
+        $this->process->start();
+        $this->process->wait();
+    }
 }
